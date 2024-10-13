0a5cb1fc34ded4da821f2a5903204309d3c1db11
uvsq22405217 <ramdanikhaled1410@gmail.com>
2024-10-13 18:04:19 +0200

    fraction constructeurs ...

--- Fraction.java
+++ Fraction.java
@@ -1,7 +1,25 @@
-public class Fraction{
+public class Fraction {
+    private int numérateur;
+    private int dénominateur;
 
+    // Constructeur avec numérateur et dénominateur
+    public Fraction(int numérateur, int dénominateur) {
+        this.numérateur = numérateur;
+        this.dénominateur = dénominateur;
+    }
+
+    // Constructeur avec juste le numérateur (dénominateur = 1)
+    public Fraction(int numérateur) {
+        this(numérateur, 1);
+    }
+
+    // Constructeur sans argument (numérateur = 0, dénominateur = 1)
+    public Fraction() {
+        this(0, 1);
+    }
+    
     @Override
-    public String toString(){
-        return "je suis une Fraction";
+    public String toString() {
+        return numérateur + "/" + dénominateur;
     }
 }
